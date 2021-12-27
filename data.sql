b95f6d0f33e5eea22b211f2b8aa673ae7dd170d2
Ortem <kav-2002@bk.ru>
2021-12-27 13:07:53 +0300

    Lab6

---
+++ data.sql
@@ -0,0 +1,14 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com'),
+('Семен', 'Петрович', 'Сидоров', 'sidorov2@my.example.com'),
+('Аркадий', 'Петрович', 'Сидоров', 'sidorov3@my.example.com'),
+('Инокентий', 'Петрович', 'Сидоров', 'sidorov4@my.example.com'),
+('Акакий', 'Петрович', 'Сидоров', 'sidorov5@my.example.com'),
+('Лука', 'Петрович', 'Сидоров', 'sidorov6@my.example.com'),
+('Святозар', 'Петрович', 'Сидоров', 'sidorov7@my.example.com'),
+('Ярополк', 'Петрович', 'Сидоров', 'sidorov8@my.example.com'),
+('Доброжир', 'Петрович', 'Сидоров', 'sidorov9@my.example.com'),
+('Ратибор', 'Петрович', 'Сидоров', 'sidorov@my.example.com'),
+('Артём', 'Валериевич', 'Кондрашин', 'kav-2002@bk.ru');
