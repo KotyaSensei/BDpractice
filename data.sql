14b62c42606c5a3146ae2f14d8958cee843a24ed
Ortem <kav-2002@bk.ru>
2022-01-20 22:16:19 +0300

    lab 11-12

--- data.sql
+++ data.sql
@@ -10,6 +10,36 @@ insert into usr (first_name, middle_name, last_name, e_mail, role_name, birth_da
 ('Люня', 'Петрович', 'Сидоров', 'sidorov71@my.example.com', 'user', '2003-07-17'),
 ('Люля', 'Петрович', 'Сидоров', 'sidorov81@my.example.com', 'user', '2003-07-17');
 
+insert into usr (first_name, middle_name, last_name, e_mail, role_name, birth_date) values
+('Артемий', 'Иванович', 'Иванов', 'ivanov1111@my.example.com', 'user', '2003-07-17'),
+('Артя', 'Петрович', 'Петров', 'petrov111@my.example.com', 'user', '2004-07-17'),
+('Артюня', 'Петрович', 'Сидоров', 'sidorov11@my.example.com', 'user', '2005-07-17'),
+('Тюня', 'Петрович', 'Сидоров', 'sidorov211@my.example.com', 'user', '2006-07-17'),
+('Артюха', 'Петрович', 'Сидоров', 'sidorov311@my.example.com', 'user', '2007-07-17'),
+('Артюша', 'Петрович', 'Сидоров', 'sidorov411@my.example.com', 'user', '2008-07-17'),
+('Тюша', 'Петрович', 'Сидоров', 'sidorov511@my.example.com', 'user', '2009-07-17'),
+('Артемка', 'Петрович', 'Сидоров', 'sidorov611@my.example.com', 'user', '2010-07-17'),
+('Артемчик', 'Петрович', 'Сидоров', 'sidorov711@my.example.com', 'user', '2010-07-17'),
+('Тема', 'Петрович', 'Сидоров', 'sidorov811@my.example.com', 'user', '2010-07-17');
+
+insert into department (id, name) values
+(1, 'Гриффиндор'),
+(2, 'Пуффендуй'),
+(3, 'Когтевран'),
+(4, 'Слизерин'),
+(5, 'Бизнес-информатика');
+
+insert into groups (id, name, course) values
+(1, 'БИ-б-о 201', 2),
+(2, 'Слизерин 211', 1),
+(3, 'Когтевран 191', 3),
+(4, 'Пуффендуй 201', 2),
+(5, 'Валеология 021', 21);
+
+insert into position (id, name) values
+(1, 'Администрация'),
+(2, 'Преподаватели'),
+(3, 'Студенты');
 
 
 
